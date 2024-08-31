c1780d03e803e1bdf79ea740df20128331ece421
shane newsom <shaned@pm.me>
2024-08-31 13:26:57 -0500

    Updated logic on the on the save.

--- ASecureNoteMaker/MainPage.xaml.cs
+++ ASecureNoteMaker/MainPage.xaml.cs
@@ -1,13 +1,5 @@
-﻿using Microsoft.Maui.Controls;
-using System.IO;
-using Microsoft.Maui.Storage;
-using System;
-using Microsoft.Maui.Controls;
-using System.IO;
-using Microsoft.Maui.Storage;
-
-
-
+﻿using ASecureNoteMaker.Extensions;
+using CommunityToolkit.Maui.Storage;
 
 namespace ASecureNoteMaker
 {
@@ -15,25 +7,58 @@ namespace ASecureNoteMaker
     {
         int count = 0;
 
-        string passphrase = "YourSecurePassphrase"; // Use a strong passphrase
-        string encryptedFilePath = Path.Combine(@"c:\temp\", "encryptedfile.txt");
+        string passphrase = string.Empty; //"YourSecurePassphrase"; // Use a strong passphrase
+        string encryptedFilePath = string.Empty; //Path.Combine(@"c:\temp\", "encryptedfile.txt");
 
         private IDispatcherTimer autoSaveTimer;
 
-
-
+        // Base Functions
         public MainPage()
         {
             InitializeComponent();
-            //encryptedFilePath = String.Empty;
             this.Loaded += OnPageLoaded;
         }
 
+        private async void OnPageLoaded(object sender, EventArgs e)
+        {
+
+
+            //When add seetings... need to reade the settings file to get default file.
+            //string result = await DisplayPromptAsync("Input", "Please enter some text:");
+
+            //if (!string.IsNullOrEmpty(result))
+            //{
+            //    passphrase = result;
+            //}
+
+            //OpenFile_Clicked(null, null);
+        }
+
+        private void AutoSave_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                // Start auto-save
+                autoSaveTimer = Dispatcher.CreateTimer();
+                autoSaveTimer.Interval = TimeSpan.FromSeconds(30);
+                autoSaveTimer.Tick += (s, e) => SaveText_ClickedAsync(this, EventArgs.Empty);
+                autoSaveTimer.Start();
+            }
+            else
+            {
+                // Stop auto-save
+                autoSaveTimer?.Stop();
+            }
+
+        }
+
+        // Custom Functions
         private async void OpenFile_Clicked(object sender, EventArgs e)
         {
             try
             {
                 var result = await FilePicker.PickAsync();
+
                 if (result != null)
                 {
                     Note.Text = FilEncryption.DecryptFile(result.FullPath, passphrase);
@@ -46,32 +71,38 @@ namespace ASecureNoteMaker
             }
         }
 
-        private async void OnPageLoaded(object sender, EventArgs e)
+        private async void SaveText_ClickedAsync(object sender, EventArgs e)
         {
-            string result = await DisplayPromptAsync("Input", "Please enter some text:");
-
-            if (!string.IsNullOrEmpty(result))
+            if (passphrase.IsNullOrWhiteSpace())
             {
-                passphrase = result;
+                passphrase = await DisplayPromptAsync("Input", "Please enter some text:");
             }
 
-            if (!encryptedFilePath.Equals(string.Empty))
+            if (passphrase.IsNullOrWhiteSpace())
             {
-                using var stream = new MemoryStream(Encoding.Default.GetBytes(Note.Text));
-
-                var encryptedFilePath = await FileSaver.Default.SaveAsync(null, stream, CancellationToken.None);
-
+                await DisplayAlert("Blank value", "Can't have blank text in the password", "Ok");
+                return;
             }
 
-        private void SaveText_Clicked(object sender, EventArgs e)
-        {
-            FilEncryption.EncryptFile(Note.Text, encryptedFilePath, passphrase);
+            
+            var fileSaverResult = await FileSaver.Default.SaveAsync("Newfile.txt", new MemoryStream(), CancellationToken.None);
+
+            if (fileSaverResult == null)
+            {
+                await DisplayAlert("Blank value", "No locatoin found or used.", "Ok");
+                return;
+            }
+
+            FilEncryption.EncryptFile(Note.Text, fileSaverResult.FilePath, passphrase);
+
+
             MainPageStatus.Text = $"Note Saved in {encryptedFilePath}";
         }
 
         private async void NewFile_Clicked(object sender, EventArgs e)
         {
             bool NewFile = await DisplayAlert("Confirmation", "Are you sure you want to start a new file?", "Yes", "No");
+
             if (NewFile)
             {
                 Note.Text = string.Empty;
@@ -86,24 +117,6 @@ namespace ASecureNoteMaker
                 // Close the application
                 Application.Current.Quit();
             }
-        }
-
-        private void AutoSave_CheckedChanged(object sender, CheckedChangedEventArgs e)
-        {
-            if (e.Value)
-            {
-                // Start auto-save
-                autoSaveTimer = Dispatcher.CreateTimer();
-                autoSaveTimer.Interval = TimeSpan.FromSeconds(30);
-                autoSaveTimer.Tick += (s, e) => SaveText_Clicked(this, EventArgs.Empty);
-                autoSaveTimer.Start();
-            }
-            else
-            {
-                // Stop auto-save
-                autoSaveTimer?.Stop();
-            }
-
         }
 
     }
